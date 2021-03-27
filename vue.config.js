9e30ab0bc6b1159abaf17aec2912ad66f9989a80
Ana Hernandez <ana.hernandez_ext@mkdautomotive.com>
2021-03-27 13:02:09 -0400

    fix config chainWebpack

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,17 @@ module.exports = {
   chainWebpack: config => {
     config
     .plugins.delete('prefetch')
+    config
     .plugin('html')
-    .plugin('preload').tap((options) => {
-      options[0].include = 'allChunks'
-      return options
-    })
     .tap(args => {
       args[0].title = 'Ana Hernández | Developer'
       return args
     })
+    config
+    .plugin('preload').tap((options) => {
+      options[0].include = 'allChunks'
+      return options
+    })
+    
   }
 }
